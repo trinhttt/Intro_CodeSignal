e40f4e1f4a476c4f5ecefb8bba19bdd6de07020b
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2020-03-20 22:15:13 +0700

    Init project

---
+++ Intro_CodeSignal/Intro_CodeSignal/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Intro_CodeSignal
+//
+//  Created by Trinh Thai on 3/20/20.
+//  Copyright © 2020 Trinh Thai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
