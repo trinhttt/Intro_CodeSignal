9571767d6c8b8da2e1038d40fe1a1e4496aa2ec9
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2020-03-21 10:02:13 +0700

    Task 5

--- Intro_CodeSignal/Intro_CodeSignal/ViewController.swift
+++ Intro_CodeSignal/Intro_CodeSignal/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print(shapeArea(n: 3))
     }
 
     // MARK: - Task 2
@@ -32,6 +33,35 @@ class ViewController: UIViewController {
         }
         return res.max()!
     }
+    
+    // MARK: - Task 5
+    func shapeArea(n: Int) -> Int {
+        guard n > 1 else { return 1 }
+        let w = n * 2 - 1
+        var area = w * w
+        for i in 1...n - 1 {
+            area = area - i * 4
+        }
+        return area
+    }
+
+    func BEST_shapeArea(n: Int) -> Int {
+        
+        return (n * n) + ((n - 1) * (n - 1))
+    }
+    
+    func shapeArea2(n: Int) -> Int {
+        return n == 1 ? 1 : shapeArea(n: n - 1) + (4 * (n - 1))
+    }
+    
+    func shapeArea3(n: Int) -> Int {
+        return (0..<n).map {4 * $0}.reduce(0, +) + 1
+    }
+    
+    func shapeArea4(n: Int) -> Int {
+     return Int( pow(Double(n), 2) + pow(Double(n - 1), 2)  )
+    }
+
 
 }
 
