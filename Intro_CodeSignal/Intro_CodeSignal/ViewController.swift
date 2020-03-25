28618b482648e830c6362bb24f4baea8743fcc4b
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2020-03-25 21:38:25 +0700

    Task 25

--- Intro_CodeSignal/Intro_CodeSignal/ViewController.swift
+++ Intro_CodeSignal/Intro_CodeSignal/ViewController.swift
@@ -9,21 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(shapeArea(n: 3))
     }
-
+    
     // MARK: - Task 2
     func centuryFromYear(year: Int) -> Int {
         return (year + 99) / 100
     }
-
+    
     // MARK: - Task 3
     func checkPalindrome(inputString: String) -> Bool {
-      return String(inputString.reversed()) == inputString
+        return String(inputString.reversed()) == inputString
     }
     
     // MARK: - Task 4
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
         }
         return area
     }
-
+    
     func BEST_shapeArea(n: Int) -> Int {
         
         return (n * n) + ((n - 1) * (n - 1))
@@ -59,9 +59,9 @@ class ViewController: UIViewController {
     }
     
     func shapeArea4(n: Int) -> Int {
-     return Int( pow(Double(n), 2) + pow(Double(n - 1), 2)  )
+        return Int( pow(Double(n), 2) + pow(Double(n - 1), 2)  )
     }
-
+    
     // MARK: - Task 6
     func makeArrayConsecutive2(statues: [Int]) -> Int {
         return statues.max()! - statues.min()! - statues.count + 1
@@ -105,7 +105,7 @@ class ViewController: UIViewController {
         
         return components.filter { $0 > -1 && $0 <= 255 }.count == 4
     }
-
+    
     // only pass 17/21 test: xem cach lam hui
     func ok2_isIPv4Address(inputString: String) -> Bool {
         let values = inputString.components(separatedBy: ".").map { Int($0) }
@@ -114,7 +114,7 @@ class ViewController: UIViewController {
     
     // MARK: - Task 22
     func avoidObstacles(inputArray: [Int]) -> Int {
-    let max = inputArray.max()!
+        let max = inputArray.max()!
         for i in 1...max {
             var temp = 0
             while temp <= max {
@@ -128,67 +128,85 @@ class ViewController: UIViewController {
                 return i
             }
         }
-    return max + 1
-}
-
-func BEST_avoidObstacles(inputArray: [Int]) -> Int {
-    var minimumJump = 1
-    while inputArray.map({ $0 % minimumJump }).contains(0) {
-        minimumJump += 1
-    }
-    return minimumJump
-}
-    
-// MARK: - Task 23
-func boxBlur(image: [[Int]]) -> [[Int]] {
-var resultArr: [[Int]] = []
-    for i in 0...image.count - 3 {
-        resultArr.append([])
-        for j in 0...image[0].count - 3 {
-            let num = calculateAverage(i: i, j: j, array: image)
-            resultArr[i].append(num)
-        }
-    }
-    return resultArr
-}
-
-func calculateAverage(i: Int, j: Int, array: [[Int]]) -> Int{
-    var sum = 0
-    for ii in i...i + 2 {
-        for jj in j...j + 2 {
-            sum += array[ii][jj]
-        }
-    }
-    return sum / 9
-}
-
-//other: NOTE: Use reduce for zArray
-func getArrayAverage(x: Int, y: Int, imageArray: [[Int]]) -> Int {
-    var average:Int = 0
-    for xArray in imageArray[y...(y+2)] {
-        average += xArray[x...(x+2)].reduce(0, +)
+        return max + 1
     }
     
-    return Int((Double(average)/9).rounded(.down))
-    
-}
-
-
-func ok_boxBlur(image: [[Int]]) -> [[Int]] {
-    let sizeX = image[0].count - 2
-    let sizeY = image.count - 2
-    var result: [[Int]] = [[Int]]()
-    for yIndex in 0..<sizeY {
-        var xArray = [Int]()
-        for xIndex in 0..<sizeX {
-            let av = getArrayAverage(x:xIndex, y:yIndex, imageArray: image)
-            xArray.append(av)
+    func BEST_avoidObstacles(inputArray: [Int]) -> Int {
+        var minimumJump = 1
+        while inputArray.map({ $0 % minimumJump }).contains(0) {
+            minimumJump += 1
         }
-        result.append(xArray)
-    } 
-    return result
-}
+        return minimumJump
+    }
+    
+    // MARK: - Task 23
+    func boxBlur(image: [[Int]]) -> [[Int]] {
+        var resultArr: [[Int]] = []
+        for i in 0...image.count - 3 {
+            resultArr.append([])
+            for j in 0...image[0].count - 3 {
+                let num = calculateAverage(i: i, j: j, array: image)
+                resultArr[i].append(num)
+            }
+        }
+        return resultArr
+    }
+    
+    func calculateAverage(i: Int, j: Int, array: [[Int]]) -> Int{
+        var sum = 0
+        for ii in i...i + 2 {
+            for jj in j...j + 2 {
+                sum += array[ii][jj]
+            }
+        }
+        return sum / 9
+    }
+    
+    //other: NOTE: Use reduce for zArray
+    func getArrayAverage(x: Int, y: Int, imageArray: [[Int]]) -> Int {
+        var average:Int = 0
+        for xArray in imageArray[y...(y+2)] {
+            average += xArray[x...(x+2)].reduce(0, +)
+        }
+        
+        return Int((Double(average)/9).rounded(.down))
+        
+    }
+    
+    
+    func ok_boxBlur(image: [[Int]]) -> [[Int]] {
+        let sizeX = image[0].count - 2
+        let sizeY = image.count - 2
+        var result: [[Int]] = [[Int]]()
+        for yIndex in 0..<sizeY {
+            var xArray = [Int]()
+            for xIndex in 0..<sizeX {
+                let av = getArrayAverage(x:xIndex, y:yIndex, imageArray: image)
+                xArray.append(av)
+            }
+            result.append(xArray)
+        }
+        return result
+    }
+    
+    // MARK: - Task 25
 
+    func arrayReplace(inputArray: [Int], elemToReplace: Int, substitutionElem: Int) -> [Int] {
+        let resultArray = inputArray.map({ (num) -> Int in
+            if num == elemToReplace {
+                return substitutionElem
+            }
+            return num
+        })
+        return resultArray
+    }
 
+    func arrayReplace1(inputArray: [Int], elemToReplace: Int, substitutionElem: Int) -> [Int]
+    {
+        return inputArray.map{$0 == elemToReplace ? substitutionElem : $0 }
+    }
+
+    
+    
 }
 
