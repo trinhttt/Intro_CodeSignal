63fea0a2932a5942d6e3896d5fe84451401d889f
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2020-03-28 22:56:37 +0700

    Task 31

--- Intro_CodeSignal/Intro_CodeSignal/ViewController.swift
+++ Intro_CodeSignal/Intro_CodeSignal/ViewController.swift
@@ -313,7 +313,17 @@ class ViewController: UIViewController {
     func ok_circleOfNumbers(n: Int, firstNumber: Int) -> Int {
         return (firstNumber + n/2) % n
     }
-
+    
+    // MARK: - Task 31
+    func depositProfit(deposit: Int, rate: Int, threshold: Int) -> Int {
+        var yearCount = 0
+        var currentAmount = Double(deposit)
+        while currentAmount < Double(threshold) {
+            currentAmount += currentAmount * (Double(rate) / 100)
+            yearCount += 1
+        }
+        return yearCount
+    }
 }
 
 
